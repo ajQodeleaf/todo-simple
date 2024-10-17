777df57243afdc9499268625f05c39970fc34bf9
Aradhya Jain <ajain@qodeleaf.com>
2024-10-17 15:41:07 +0530

    Feature: Integrated AuthProvider for user authorization functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,13 +3,16 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
 import theme from "../styles/theme";
 import { ThemeProvider } from "@/context/themeContext";
+import { AuthProvider } from "@/context/userAuthenticationContext";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <ChakraProvider theme={theme}>
-          <ThemeProvider>{children}</ThemeProvider>
+          <AuthProvider>
+            <ThemeProvider>{children}</ThemeProvider>
+          </AuthProvider>
         </ChakraProvider>
       </body>
     </html>
