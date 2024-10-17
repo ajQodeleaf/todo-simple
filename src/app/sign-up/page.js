a9497e306eb4295a5cc878fed5640143f9a755c1
Aradhya Jain <ajain@qodeleaf.com>
2024-10-17 15:45:16 +0530

    Feat: Sign Up page

--- src/app/sign-up/page.js
+++ src/app/sign-up/page.js
@@ -1,4 +1,5 @@
 "use client";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import {
   Flex,
   Box,
@@ -15,11 +16,72 @@ import {
   useColorModeValue,
   Link,
 } from "@chakra-ui/react";
-import { useState } from "react";
-import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { useRouter } from "next/navigation";
+import { useState, useContext } from "react";
+import { AuthContext } from "@/context/userAuthenticationContext";
+
 
 export default function SignupCard() {
+  const router = useRouter();
   const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({ email: "", password: "" });
+
+  const { login } = useContext(AuthContext);
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+    setErrors({ ...errors, [e.target.id]: "" });
+  };
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validatePassword = (password) => {
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    return passwordRegex.test(password);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!formData.firstName || !formData.email || !formData.password) {
+      alert("Please fill in all required fields.");
+      return;
+    }
+
+    if (!validateEmail(formData.email)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "Please enter a valid email address.",
+      }));
+      return;
+    }
+
+    if (!validatePassword(formData.password)) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        password:
+          "Password must be at least 8 characters long and contain both letters and numbers.",
+      }));
+      return;
+    }
+
+    const userData = {
+      name: `${formData.firstName} ${formData.lastName}`,
+      email: formData.email,
+      token: "sampleToken123",
+    };
+    login(userData);
+    router.push('/home');
+  };
 
   return (
     <Flex
@@ -39,29 +101,50 @@ export default function SignupCard() {
           boxShadow={"lg"}
           p={8}
         >
-          <Stack spacing={4}>
+          <Stack as="form" onSubmit={handleSubmit} spacing={4}>
             <HStack>
               <Box>
                 <FormControl id="firstName" isRequired>
                   <FormLabel>First Name</FormLabel>
-                  <Input type="text" />
+                  <Input
+                    type="text"
+                    value={formData.firstName}
+                    onChange={handleInputChange}
+                  />
                 </FormControl>
               </Box>
               <Box>
                 <FormControl id="lastName">
                   <FormLabel>Last Name</FormLabel>
-                  <Input type="text" />
+                  <Input
+                    type="text"
+                    value={formData.lastName}
+                    onChange={handleInputChange}
+                  />
                 </FormControl>
               </Box>
             </HStack>
             <FormControl id="email" isRequired>
               <FormLabel>Email address</FormLabel>
-              <Input type="email" />
+              <Input
+                type="email"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+              {errors.email && (
+                <Text color="red.500" fontSize="sm">
+                  {errors.email}
+                </Text>
+              )}
             </FormControl>
             <FormControl id="password" isRequired>
               <FormLabel>Password</FormLabel>
               <InputGroup>
-                <Input type={showPassword ? "text" : "password"} />
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  value={formData.password}
+                  onChange={handleInputChange}
+                />
                 <InputRightElement h={"full"}>
                   <Button
                     variant={"ghost"}
@@ -73,6 +156,11 @@ export default function SignupCard() {
                   </Button>
                 </InputRightElement>
               </InputGroup>
+              {errors.password && (
+                <Text color="red.500" fontSize="sm">
+                  {errors.password}
+                </Text>
+              )}
             </FormControl>
             <Stack spacing={10} pt={2}>
               <Button
@@ -83,6 +171,7 @@ export default function SignupCard() {
                 _hover={{
                   bg: "blue.500",
                 }}
+                type="submit"
               >
                 Sign up
               </Button>
