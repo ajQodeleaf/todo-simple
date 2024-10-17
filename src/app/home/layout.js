6930d38fe23d4a9e73872901d748c4e66bdeac9a
Aradhya Jain <ajain@qodeleaf.com>
2024-10-17 15:46:18 +0530

    Refactor: Home Page Layout

--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -1,14 +1,14 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import SideNavBar from "../../components/SideNavBar.js";
+import SideNavBar from "@/components/SideNavBar";
 
 export default function HomeLayout({ children }) {
   return (
     <>
       <Header />
-      <div style={{ display: "flex" }}>
-        <SideNavBar />
-        <main>{children}</main>
+      <SideNavBar />
+      <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
+        <main style={{ flexGrow: 1, width: "100%" }}>{children}</main>
       </div>
       <Footer />
     </>
