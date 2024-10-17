adf13d4ac4de3efb64bbb60c5f54e25652ee8636
Aradhya Jain <ajain@qodeleaf.com>
2024-10-17 15:44:46 +0530

    Feature: SideNavBar for home page

--- src/components/SideNavBar.js
+++ src/components/SideNavBar.js
@@ -1,148 +1,83 @@
 "use client";
 
-import React from "react";
 import {
-  IconButton,
   Box,
-  CloseButton,
   Flex,
-  Icon,
-  useColorModeValue,
-  Text,
+  VStack,
+  IconButton,
   Drawer,
+  DrawerBody,
+  DrawerOverlay,
   DrawerContent,
+  DrawerCloseButton,
+  Text,
+  Divider,
   useDisclosure,
+  useColorModeValue,
 } from "@chakra-ui/react";
-import {
-  FiHome,
-  FiTrendingUp,
-  FiCompass,
-  FiStar,
-  FiSettings,
-  FiMenu,
-} from "react-icons/fi";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { FiHome, FiSettings, FiLogOut } from "react-icons/fi";
 
-const LinkItems = [
-  { name: "Home", icon: FiHome },
-  { name: "Trending", icon: FiTrendingUp },
-  { name: "Explore", icon: FiCompass },
-  { name: "Favourites", icon: FiStar },
-  { name: "Settings", icon: FiSettings },
-];
-
-export default function SimpleSidebar() {
+const SideNavBar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
-      <SidebarContent
-        onClose={() => onClose}
-        display={{ base: "none", md: "block" }}
+    <>
+      <IconButton
+        icon={<HamburgerIcon />}
+        aria-label="Open Menu"
+        onClick={onOpen}
+        variant="outline"
+        borderRadius="50%"
+        boxShadow="sm"
+        position="fixed"
+        top={4}
+        left={16}
+        zIndex={10}
       />
-      <Drawer
-        isOpen={isOpen}
-        placement="left"
-        onClose={onClose}
-        returnFocusOnClose={false}
-        onOverlayClick={onClose}
-        size="full"
-      >
-        <DrawerContent>
-          <SidebarContent onClose={onClose} />
+
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent
+          bg={useColorModeValue("white", "gray.800")}
+          borderRightRadius="md"
+          boxShadow="lg"
+        >
+          <DrawerCloseButton />
+          <DrawerBody>
+            <Flex direction="column" height="100%" p={4}>
+              <Text fontSize="2xl" fontWeight="bold" mb={4}>
+                Menu
+              </Text>
+              <Divider />
+              <VStack align="start" spacing={4} mt={4}>
+                <MenuItem icon={FiHome} label="Home" />
+                <MenuItem icon={FiSettings} label="Settings" />
+                <MenuItem icon={FiLogOut} label="Logout" />
+              </VStack>
+            </Flex>
+          </DrawerBody>
         </DrawerContent>
       </Drawer>
-      <Box ml={{ base: 0, md: 60 }} p="4">
-        {/* Content */}
-      </Box>
-    </Box>
-  );
-}
-
-const SidebarContent = ({ onClose, ...rest }) => {
-  return (
-    <Box
-      bg={useColorModeValue("white", "gray.900")}
-      borderRight="1px"
-      borderRightColor={useColorModeValue("gray.200", "gray.700")}
-      w={{ base: "full", md: 60 }}
-      pos="fixed"
-      h="full"
-      {...rest}
-    >
-      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
-        <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-          Logo
-        </Text>
-        <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
-      </Flex>
-      {LinkItems.map((link) => (
-        <NavItem key={link.name} icon={link.icon}>
-          {link.name}
-        </NavItem>
-      ))}
-    </Box>
+    </>
   );
 };
 
-const NavItem = ({ icon, children, ...rest }) => {
-  return (
-    <Box
-      as="a"
-      href="#"
-      style={{ textDecoration: "none" }}
-      _focus={{ boxShadow: "none" }}
-    >
-      <Flex
-        align="center"
-        p="4"
-        mx="4"
-        borderRadius="lg"
-        role="group"
-        cursor="pointer"
-        _hover={{
-          bg: "cyan.400",
-          color: "white",
-        }}
-        {...rest}
-      >
-        {icon && (
-          <Icon
-            mr="4"
-            fontSize="16"
-            _groupHover={{
-              color: "white",
-            }}
-            as={icon}
-          />
-        )}
-        {children}
-      </Flex>
-    </Box>
-  );
-};
-
-const MobileNav = ({ onOpen, ...rest }) => {
+const MenuItem = ({ icon, label }) => {
   return (
     <Flex
-      ml={{ base: 0, md: 60 }}
-      px={{ base: 4, md: 24 }}
-      height="20"
-      alignItems="center"
-      bg={useColorModeValue("white", "gray.900")}
-      borderBottomWidth="1px"
-      borderBottomColor={useColorModeValue("gray.200", "gray.700")}
-      justifyContent="flex-start"
-      {...rest}
+      align="center"
+      p={2}
+      borderRadius="md"
+      _hover={{ bg: "gray.100", color: "black" }}
+      transition="background 0.2s"
     >
-      <IconButton
-        variant="outline"
-        onClick={onOpen}
-        aria-label="open menu"
-        icon={<FiMenu />}
-      />
-
-      <Text fontSize="2xl" ml="8" fontFamily="monospace" fontWeight="bold">
-        Logo
+      <Box as={icon} mr={2} fontSize="20px" />
+      <Text fontSize="lg" _hover={{ color: "black" }} transition="color 0.2s">
+        {label}
       </Text>
     </Flex>
   );
 };
+
+export default SideNavBar;
