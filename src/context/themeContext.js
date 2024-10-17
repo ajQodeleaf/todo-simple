ed492fdefcbe977283179d9c57dd66d61225e31c
Aradhya Jain <ajain@qodeleaf.com>
2024-10-17 10:25:18 +0530

    Context: Theme Provider

---
+++ src/context/themeContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    console.log("Theme set to:- ", theme);
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  return useContext(ThemeContext);
+}
