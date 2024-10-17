b70887ce1eea42fcd14ae5f307707193f6f9ecaf
Aradhya Jain <ajain@qodeleaf.com>
2024-10-17 10:26:51 +0530

    Add: Theme Provider to the app

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,15 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
 import theme from "../styles/theme";
+import { ThemeProvider } from "@/context/themeContext";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <ChakraProvider theme={theme}>{children}</ChakraProvider>
+        <ChakraProvider theme={theme}>
+          <ThemeProvider>{children}</ThemeProvider>
+        </ChakraProvider>
       </body>
     </html>
   );
